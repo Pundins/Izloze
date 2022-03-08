f69e9d5767be048c2c30f569b359a071e5753164
Pundins <mpundinsyt@gmail.com>
2022-03-08 10:18:18 +0200

    Pirmais

---
+++ script.js
@@ -0,0 +1,6 @@
+const vardi = ['Jana Krūmiņa', 'Jānis Bērziņš', 'Pēteris Liepa', 'Laura Ose', 'Artis priede'];
+const balvas = ['Dators', 'Telefons', 'Leduskapis', 'Fotoaparāts', 'Kafijas autumāts'];
+const maudaKopa = 1000000;
+let uzvaretajuSkaits = 5;
+
+console.log (vardi[rand]);
