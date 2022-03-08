b439a68233b1c3c5f8b3eafccebdace026bea54a
Pundins <mpundinsyt@gmail.com>
2022-03-08 13:15:48 +0200

    stunda

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 const vardi = ['Jana Krūmiņa', 'Jānis Bērziņš', 'Pēteris Liepa', 'Laura Ose', 'Artis priede'];
 const balvas = ['Dators', 'Telefons', 'Leduskapis', 'Fotoaparāts', 'Kafijas autumāts'];
-const maudaKopa = 1000000;
+const naudaKopa = 1000000;
 let uzvaretajuSkaits = 5;
 
-for (let i=0; i<uzvaretajuSkaits; i++){
+let rindas = document.querySelector("rindas"); //pievieno mainīgo
+rindas.innerHTML = "";
 
-let rand = Math.random() * vardi.length;
-rand = Math.floor(rand);
-let uzvaretajs = vardi[rand];
-let rindas = document.querySelector('.rindas'); //pievieno mainīgo
-rindas.innerHTML +=` //pievieno +, lai pieskatītu nākamo
+for (let i = 0; i < uzvaretajuSkaits; i++) {
+
+    let rand = Math.random() * vardi.length;
+    rand = Math.floor(rand);
+    let uzvaretaji = vardi[rand];
+    let laimesti = balvas[rand];
+
+
+    rindas.innerHTML += `
 <tr>
-    <td>${i+1}</td> // $norāda mainīgo
-    <td>${uzvaretajs}</td>
-</tr>` //izveide vairākās rinās
+<td>${i + 1}</td>
+<td>${uzvaretaji}</td>
+<td>${laimesti}</td>
+</tr>`
 
 }
 
